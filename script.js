daab148557af7ebc5ed29b501ebbef440a4f5729
Michael Okolo <michaelokolo62@yahoo.com>
2023-05-21 17:41:23 -0400

    working on styling the various sections

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu')
-console.log(hamburger)
+const green = document.querySelector('.green')
+
 
 hamburger.addEventListener("click", ()=>{
     !navMenu.classList.contains("active") ? navMenu.classList.add('active') : navMenu.classList.remove('active')
     !hamburger.classList.contains("active") ? hamburger.classList.add('active') : hamburger.classList.remove('active')
+})
+
+green.classList.add('overlay')
+green.addEventListener('mouseover', ()=>{
+    green.classList.remove('overlay')
+})
+green.addEventListener('mouseout', ()=>{
+    green.classList.add('overlay')
 })
