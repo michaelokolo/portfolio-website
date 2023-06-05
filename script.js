d7f657be18e1fe1e4164aa4435553e9a38c130bf
Okolo Michael <Okolo.m@ieslglobal.com>
2023-06-05 14:45:28 +0100

    added animation to the page

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu')
 const navMenuItems = document.querySelectorAll('.nav-menu ul li')
 const green = document.querySelector('.green')
+const reveals = document.querySelectorAll('.reveal')
 
 
 hamburger.addEventListener("click", ()=>{
@@ -17,3 +18,18 @@ navMenuItems.forEach(item=>{
         hamburger.classList.contains("active") && hamburger.classList.remove('active')
     }
 })
+
+function reveal(){
+    reveals.forEach((reveal)=>{
+       var windowHeight = window.innerHeight;
+        var elementTop = reveal.getBoundingClientRect().top;
+        var elementVisible = 150;
+        elementTop < windowHeight - elementVisible ?
+        reveal.classList.add('active'):
+        reveal.classList.remove('active')
+    })
+}
+
+window.addEventListener('scroll', reveal);
+
+reveal();
