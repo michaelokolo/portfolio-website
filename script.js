2b48291d4f611dcc4fb7a0104f2f35cfa2c49eac
Michael Okolo <michaelokolo62@yahoo.com>
2023-06-12 04:40:33 -0400

    completed the typing and deleting section

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const navMenuItems = document.querySelectorAll('.nav-menu ul li')
 const green = document.querySelector('.green')
 const reveals = document.querySelectorAll('.reveal')
 const homeElement = document.querySelector('.home')
-const text = 'I build things for the web.'
-let container = document.querySelector('.home div h3')
+let container = document.querySelector('.home div h3 .text')
 
 hamburger.addEventListener("click", ()=>{
     !navMenu.classList.contains("active") ? navMenu.classList.add('active') : navMenu.classList.remove('active')
@@ -47,27 +46,53 @@ homeElement.animate(
     }
 )
 
-let i = 0;
-function typewriter(){
+const carouselText = [
+    {text: "build things for the web.", color:'red'},
+    {text: "'m a front-end developer.", color:'orange'},
+    {text: "'m a back-end developer.", color:'yellow'}
+]
+
+async function text(list, eleRef){
+    var i = 0;
+    while(true){
+        updateFontColor(eleRef, list[i].color)
+        await typeSentence(list[i].text, eleRef);
+        await waitForMs(1500);
+        await deleteSentence(eleRef);
+        await waitForMs(500);
+        i++
+        if(i >= list.length){i = 0}
+    }
+}
+
+function updateFontColor(eleRef , color){
+    eleRef.style.color = color;
+}
+text(carouselText, container)
+async function typeSentence(sentence, eleRef, delay = 100){
+    const letters = sentence.split('');
     
-    if (i < text.length){
-        container.innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typewriter, 50)
+    for(let i = 0; i < letters.length; i++){
+        await waitForMs(delay)
+        eleRef.append(letters[i])
     }
-    if(container.innerHTML === text){
-        setTimeout(Delete, 50)
-    }
-   
-}
-function Delete(){
-        let gigg = text.substring(0, text.length - 1);
-        console.log(gigg)
-        text.length--
+    return;
 }
 
-typewriter()
-Delete()
+
+function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 
+
+async function deleteSentence(eleRef){
+    const sentence = eleRef.innerHTML;
+    const letters = sentence.split('');
+    for(let i = letters.length; i > 0 ; i--){
+        await waitForMs(100);
+        letters.pop();
+        eleRef.innerHTML = letters.join('');
+    }
+}
 
